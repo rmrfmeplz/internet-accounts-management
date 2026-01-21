ddf13bb86ec939f66f288f0bdf503f4acc9bf394
whoami <13218541+zzzzzzyx@user.noreply.gitee.com>
2026-01-21 15:13:16 +0800

    refactor: 统一消息提示不带结束符号

--- backend/services/InternetAccountService.js
+++ backend/services/InternetAccountService.js
@@ -4,7 +4,7 @@ const internetAccountDao = require('../dao/InternetAccountDao');
 function createInternetAccount(platformName, account) {
     const internetAccounts = internetAccountDao.listAllInternetAccounts()
     const isDuplicate = internetAccounts.some(internetAccount => internetAccount['platformName'] === platformName && internetAccount['account'] === account)
-    if (isDuplicate) throw new Error(`检测到重复数据：平台 ${platformName} 下的账号 ${account} 已存在。`)
+    if (isDuplicate) throw new Error(`检测到重复数据：平台 ${platformName} 下的账号 ${account} 已存在`)
     const id = v4().toUpperCase()
     const createTime = Date.now()
     const updateTime = createTime
