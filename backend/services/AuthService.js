1202e641856b28955660dce11477f7bd41b8238f
whoami <13218541+zzzzzzyx@user.noreply.gitee.com>
2026-01-28 10:18:52 +0800

    feat: 完成初始化密码功能

--- backend/services/AuthService.js
+++ backend/services/AuthService.js
@@ -1,12 +1,19 @@
 const authDao = require('../dao/AuthDao')
+const bcrypt = require('bcryptjs')
 
 function getIsInitialPasswordSet() {
     return authDao.findIsInitialPasswordSet()
 }
 
-function initialPassword(initialPassword) {
+async function encryptPassword(plainPassword) {
+    const saltRounds = 10
+    return await bcrypt.hash(plainPassword, saltRounds)
+}
+
+async function initialPassword(initialPassword) {
     if (authDao.findIsInitialPasswordSet()) throw new Error('Initial password setup failed. Each user can only complete the first-time password setup once')
-    authDao.updatePassword(initialPassword)
+    const hashedPassword = await encryptPassword(initialPassword)
+    authDao.updatePassword(hashedPassword)
     authDao.updateIsInitialPasswordSet(true)
 }
 
