6a17d5f317decb93b6a378a913711f7ed38044af
whoami <13218541+zzzzzzyx@user.noreply.gitee.com>
2026-01-29 17:13:31 +0800

    feat: 使用electron搭建桌面端APP

---
+++ electron/main.cjs
@@ -0,0 +1,29 @@
+const {app, BrowserWindow, Menu} = require('electron')
+
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true'
+
+let mainWindow
+
+async function createMainWindow() {
+    mainWindow = new BrowserWindow({minWidth: 1000, minHeight: 600, title: '管账易'})
+    await mainWindow.loadURL('http://localhost:5173')
+    Menu.setApplicationMenu(null)
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
+}
+
+app.whenReady().then(async () => {
+    await createMainWindow()
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
