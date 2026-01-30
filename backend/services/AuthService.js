49bbe86d4272d2d6217b7b74523afb1dbd50a3bb
whoami <13218541+zzzzzzyx@user.noreply.gitee.com>
2026-01-30 16:59:38 +0800

    chore: 为JWT过期时间配置添加兜底默认值1h

--- backend/services/AuthService.js
+++ backend/services/AuthService.js
@@ -19,7 +19,7 @@ async function initialPassword(initialPassword) {
 }
 
 const JWT_SECRET = process.env.JWT_SECRET
-const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '1h'
 
 async function login(username, password) {
     const auth = authDao.findAuth()
