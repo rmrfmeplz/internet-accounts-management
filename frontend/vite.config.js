399b9cb1ae93a15110ad1b16a57dccd437df6f01
whoami <13218541+zzzzzzyx@user.noreply.gitee.com>
2026-01-30 16:36:52 +0800

    chore: 移除vueDevTools调试工具

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import {fileURLToPath, URL} from 'node:url'
 
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
@@ -12,7 +11,6 @@ import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
 export default defineConfig({
     plugins: [
         vue(),
-        vueDevTools(),
         AutoImport({
             imports: [
                 'vue',
